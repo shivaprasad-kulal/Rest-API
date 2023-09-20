0d7110a8174063ec3b5b1c7a8d1f6d08e94763b3
shivaprasad-kulal <shivaprasadkulal2000@gmail.com>
2023-09-20 12:02:44 +0530

    rest-api-1.1

--- Routers/UserRouter.js
+++ Routers/UserRouter.js
@@ -1,6 +1,35 @@
 const express=require("express")
 const Router=express.Router();
-Router.get("/",(req,resp)=>{
-    resp.send("get request from api1")
+const Users=require("../model/Userschema");
+Router.get("/users",async (req,resp)=>{
+    try{
+    const userlist=await Users.find();
+    resp.json(userlist);
+
+    }
+    catch(err)
+    {
+        resp.send("ERROR",err);
+    }
+
 })
+
+Router.post("/users",async (req,resp)=>{
+    const user= new Users({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password
+    })
+    try{
+    const newuser=await user.save();
+    resp.json(newuser);
+
+    }
+    catch(err)
+    {
+        resp.send("ERROR",err);
+    }
+
+}
+)
 module.exports=Router
--- Server.js
+++ Server.js
@@ -1,14 +1,22 @@
 const express=require("express")
 const mongoose=require("mongoose");
+const url="mongodb://localhost/Users"
+mongoose.connect(url,{useNewUrlParser:true})
+const con=mongoose.connection;
+con.on('open',()=>{
+    console.log("MONGODB connected..........")
+});
+
 
 const Server=express();
 const PORT=process.env.PORT || 3000;
 const Router=require('./Routers/UserRouter')
-
+Server.use(express.json());
 Server.use('/api',Router)
 
 
+
 Server.listen(PORT,()=>{
-    console.log(`server strated on port ${PORT}`)
+    console.log(`server strated on port ${PORT}............`)
     
 })
